bf438f67b513ad337af3ca9261010270dbd9156f
Ronnie Simonelli <ronnie.simonelli@gmail.com>
2015-08-23 21:04:30 -0400

    WIP

--- server/ownedcards.js
+++ server/ownedcards.js
@@ -15,24 +15,21 @@ Meteor.startup(function () {
   var cards = response_example["user_cards"];
 
   console.log("//Ownedcards:");
-  for (card in cards){
-    var numCardsOwned = cards[card]["num_owned"]; 
 
-    //we know user has this card
+  // look up name for each card_id(nested/upgrade card) / id (main/super card)
+  // each id with either correspond to either 'id' or 'card_id' in the xml file
+  // 'card_id' is a nested object
+  for (cardId in cards){
+    var numCardsOwned = cards[cardId]["num_owned"]; 
+    //only lookup names for cards I own
     if (numCardsOwned > 0){
-      console.log("User has "+numCardsOwned + " of " + card+"'s");
-      
-      //query for either card_id or id to obtain name
-      //name via card_id
-      //var nodes_card_id = xpath.select('//unit[upgrade/card_id[text()="'+card+'"]]/name/text()', doc);
-      console.log(nodes_card_id[0]);
-
-      //var nodes_level = xpath.select('//upgrade[card_id[text()="'+card+'"]]/level/text()', doc);
-      if( typeof nodes_card_id[0] == 'undefined' ) {
-        var nodes_card = xpath.select('//unit[@id="'+card+'"]/name/text()', doc);
-        console.log(nodes_card[0]);
-      } else {
-        console.log(nodes_card_id[0] + '-' + nodes_level[0] +"("+ numCardsOwned +")");
+      //check to see if 'cardId' corresponds to 'id' or 'card_id' in the xml data
+      var super_card = xpath.select('//unit[upgrade/card_id[text()="'+cardId+'"]]/name/text()', doc);
+      if(super_card[0]){// if it represents the xml 'id'
+        console.log(super_card[0]["data"] + "\t" + "(" + numCardsOwned + ")" );
+      }else{ //for card_id
+        outer_card_object = xpath.select('//unit[id[text()="'+cardId+'"]]/name/text()', doc);
+        console.log(outer_card_object[0]["data"] + "\t" + "(" + numCardsOwned + ")" );
       }
     }
   }
