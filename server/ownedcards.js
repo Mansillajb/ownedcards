8d09b614224d432a423e0a6b951a93d6b56e9098
Ronnie Simonelli <ronnie.simonelli@gmail.com>
2015-09-29 22:11:29 -0400

    wip for checking type of upgrades

--- server/ownedcards.js
+++ server/ownedcards.js
@@ -7,36 +7,38 @@ Meteor.startup(function () {
     console.log(Cards.findOne());
   }
   var sum = 0
-  var totalCards = raw_cards["unit"];
+    var totalCards = raw_cards["unit"];
 
   for (index in totalCards){
-  	var card = totalCards[index];
-  	var name = card.name;
+    var card = totalCards[index];
+    var name = card.name;
 
-  	var baseCard = {
-		_id:    card.id,
-		name:   name,
-		level:  '1'
-  	};
-  	console.log("baseCard:");
-  	// console.log(baseCard);
-  	// Cards.insert(baseCard);
+    var baseCard = {
+      _id:    card.id,
+      name:   name,
+      level:  '1'
+    };
+    // Cards.insert(baseCard);
 
-  	var upgrades = totalCards[index]["upgrade"];
-  	console.log(typeof upgrades)
+    var upgrades = totalCards[index]["upgrade"];
+    console.log("upgrades type =====");
+    console.log(typeof upgrades);
+      console.log("NEXT LINK: ARRAY CHECK");
+      if( typeof upgrades == "Array" ){
+        console.log("IS ARRAY");
+      }
 
-  	for (index in upgrades) {
-  		var card = upgrades[index];
-  		console.log(card)
-		var subCard = {
-			_id:    card.card_id,
-			name:   name,
-			level:  card.level
-		};
-		// console.log("subCard");
-		// console.log(subCard);	
-		// Cards.insert(subCard);
-  	}
+    for (index in upgrades) {
+      var card = upgrades[index];
+      var subCard = {
+        _id:    card.card_id,
+        name:   name,
+        level:  card.level
+      };
+      // console.log("subCard");
+      // console.log(subCard);	
+      // Cards.insert(subCard);
+    }
   };
 
 
